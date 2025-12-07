0e21408dc397a1176ff596d4e9421034c6aa51ec
BobesCZ <bobes.chaos@gmail.com>
2025-12-07 17:40:23 +0100

    Demo: Theme Switcher

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,10 +1,13 @@
 import { BggCollectionGame } from '@code-bucket/board-game-geek';
 import { ReactNode } from 'react';
 import { GameListRecord } from '@/admin/actions';
+import { AppThemeType } from '@/theme/config';
 import { Game } from '@/types';
 import { MyBggData } from './hooks';
 
 export type AppStoreValue = {
+  appTheme: AppThemeType;
+  changeAppTheme: (id: string) => void;
   gameList: Game[];
   activeGameListRecord: GameListRecord | undefined;
   myBggData: MyBggData | undefined;
