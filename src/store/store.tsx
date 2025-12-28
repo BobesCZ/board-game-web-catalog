a70d8803ee1457681b7e7d15b8b0e82e0c8b47c7
BobesCZ <bobes.chaos@gmail.com>
2025-12-28 21:35:30 +0100

    Demo: Theme Switcher

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { createContext, useContext } from 'react';
+import { useLocalStorage } from 'usehooks-ts';
+import { APP_THEME_DEFAULT, APP_THEME_OPTIONS } from '@/theme/config';
 import { useMyBggData } from './hooks';
 import { AppStoreProviderProps, AppStoreValue } from './types';
 import { getMergedGameList } from './utils';
@@ -11,6 +14,8 @@ const AppContext = createContext<AppStoreValue>({
   myBggData: undefined,
   onMyBggDataChange: () => undefined,
   onMyBggDataClear: () => undefined,
+  appTheme: APP_THEME_DEFAULT,
+  changeAppTheme: () => undefined,
 });
 
 export const AppStoreProvider = ({ children, activeGameListRecord }: AppStoreProviderProps) => {
@@ -19,8 +24,19 @@ export const AppStoreProvider = ({ children, activeGameListRecord }: AppStorePro
   const gameList = activeGameListRecord?.gameList ?? [];
   const mergedGameList = getMergedGameList(gameList, myBggDataProps.myBggData?.bggCollectionGameList);
 
+  const [appThemeId, setAppThemeId] = useLocalStorage('appTheme', APP_THEME_DEFAULT.id);
+  const appTheme = APP_THEME_OPTIONS.find(({ id }) => id === appThemeId) ?? APP_THEME_DEFAULT;
+  const { refresh } = useRouter();
+
+  const changeAppTheme = (id: string) => {
+    setAppThemeId(id);
+    refresh();
+  };
+
   return (
-    <AppContext.Provider value={{ activeGameListRecord, gameList: mergedGameList, ...myBggDataProps }}>
+    <AppContext.Provider
+      value={{ activeGameListRecord, gameList: mergedGameList, ...myBggDataProps, appTheme, changeAppTheme }}
+    >
       {children}
     </AppContext.Provider>
   );
