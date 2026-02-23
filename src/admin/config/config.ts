d3749334e20dcffe3b35cc48021f2ec2ae27c4ec
BobesCZ <bobes.chaos@gmail.com>
2026-02-23 21:26:51 +0100

    Demo: Allow Credentials + disable type

--- src/admin/config/config.ts
+++ src/admin/config/config.ts
@@ -20,7 +20,7 @@ export const DISABLE_USER_AUTH_ON_DEVELOPMENT = true;
  * It is always enabled on development and preview environment
  * You can disable it on production for security reasons
  */
-export const DISABLE_CREDENTIALS_ON_PRODUCTION = true;
+export const DISABLE_CREDENTIALS_ON_PRODUCTION = false;
 
 /**
  * Column options for parsing CSV
@@ -30,7 +30,7 @@ export const DISABLE_CREDENTIALS_ON_PRODUCTION = true;
  */
 export const CSV_COLUMNS_OPTIONS: CsvColumnsOptions = {
   type: {
-    enabled: true,
+    enabled: false,
     colName: 'Hra / Poznámka',
     typeGame: 'GAME',
   },
